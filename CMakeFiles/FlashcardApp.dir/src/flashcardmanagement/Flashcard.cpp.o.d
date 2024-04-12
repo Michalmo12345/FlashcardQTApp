3d5396ed889b322d8004eec90429a9425420b8d4
Szymon Jankowski <szymonjank99@gmail.com>
2024-04-12 12:16:07 +0200

    Changed gitignore and removed unnecessary files

--- CMakeFiles/FlashcardApp.dir/src/flashcardmanagement/Flashcard.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/FlashcardApp.dir/src/flashcardmanagement/Flashcard.cpp.o: \
- /home/michalmo/ZPR/flashcardapp/src/flashcardmanagement/Flashcard.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/FlashcardApp.dir/src/qt_ui/main_gui.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/FlashcardApp.dir/src/qt_ui/main_gui.cpp.o: \
- /home/michalmo/ZPR/flashcardapp/src/qt_ui/main_gui.cpp \
- /usr/include/stdc-predef.h
