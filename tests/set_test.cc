9bf9edafca70fa65296d7f422c89720114637478
mmokrzyc <01178234@pw.edu.pl>
2024-04-16 15:41:14 +0200

    implement basic tests

--- tests/set_test.cc
+++ tests/set_test.cc
@@ -14,3 +14,26 @@ TEST(SetTest, AddCard) {
     set.addCard(card);
     EXPECT_EQ(set.getCard(0), card);
 }
+
+
+TEST(SetTest, GiveRandomCard) {
+    Set set("TestSet");
+    auto card1 = std::make_shared<Flashcard>("What is the capital of France?", "Paris");
+    auto card2 = std::make_shared<Flashcard>("What is the capital of Germany?", "Berlin");
+    set.addCard(card1);
+    set.addCard(card2);
+    auto randomCard = set.giveRandomCard();
+    EXPECT_TRUE(randomCard == card1 || randomCard == card2);
+}
+
+TEST(SetTest, GetCard){
+    Set set("TestSet");
+    auto card1 = std::make_shared<Flashcard>("What is the capital of France?", "Paris");
+    auto card2 = std::make_shared<Flashcard>("What is the capital of Germany?", "Berlin");
+    set.addCard(card1);
+    set.addCard(card2);
+    EXPECT_EQ(set.getCard(0), card1);
+    EXPECT_EQ(set.getCard(1), card2);
+    EXPECT_EQ(set.getCard(2), nullptr);
+}
+
